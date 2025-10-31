948de3853a0da5382bebabb3a4e076f8e805c513
RafaelGandolfiG <rafaelgandolfi0@gmail.com>
2025-10-31 17:25:29 -0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,23 @@
+function paginadev(){
+    window.location.href= 'paginadev.html';
+}
+const btndev=document.getElementById('paginadev')
+if (btndev) btndev.onclick=paginadev;
+
+function paginaexercicio(){
+    window.location.href= 'exercicio.html';
+}
+const btnexercicio=document.getElementById('exercicio');
+if (btnexercicio) btnexercicio.onclick= paginaexercicio;
+
+function sair(){
+    window.location.href= 'index.html';
+}
+const sair1=document.getElementById('sair');
+if (sair1) sair1.onclick=sair;
+
+function prova1(){
+    window.location.href='home1.html'
+}
+const prova=document.getElementById('prova')
+if (prova) prova.onclick=prova1
