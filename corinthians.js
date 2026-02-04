b90897747244b6a9a5c6c5ad2dfc16a482acb4bd
RafaelGandolfiG <rafaelgandolfi0@gmail.com>
2026-02-04 18:11:16 -0300

    Add files via upload

---
+++ corinthians.js
@@ -0,0 +1,34 @@
+function insta(){
+    window.location.href='https://www.instagram.com/corinthians/';
+}
+
+const btninsta = document.getElementById('insta');
+if (btninsta) btninsta.onclick=insta;
+
+function voltar(){
+    window.location.href='home.html';
+}
+
+const btnvoltar = document.getElementById('voltar');
+if (btnvoltar) btnvoltar.onclick=voltar;
+
+function transfermarkt(){
+    window.location.href='https://www.transfermarkt.com.br/sc-corinthians/startseite/verein/199#google_vignette';
+}
+
+const btntr = document.getElementById('transfermarkt');
+if (btntr) btntr.onclick=transfermarkt;
+
+function youtube(){
+    window.location.href='https://www.youtube.com/@corinthians';
+}
+
+const btnytb = document.getElementById('ytb');
+if (btnytb) btnytb.onclick=youtube;
+
+function twitter(){
+    window.location.href='https://x.com/Corinthians';
+}
+
+const btnx = document.getElementById('x');
+if (btnx) btnx.onclick=twitter;
