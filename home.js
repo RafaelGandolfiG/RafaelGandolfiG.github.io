2da35903f2707fc984f14f3df62da186b06d05c3
RafaelGandolfiG <rafaelgandolfi0@gmail.com>
2026-02-09 21:37:13 -0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,79 @@
+function paginadev(){
+    window.location.href= 'paginadev.html';
+}
+const btndev=document.getElementById('paginadev')
+if (btndev) btndev.onclick=paginadev;
+
+function paginaexercicio(){
+    window.location.href= 'exercicio.html';
+}
+const btnexercicio=document.getElementById('exercicio');
+if (btnexercicio) btnexercicio.onclick= paginaexercicio;
+
+function sair(){
+    window.location.href= 'index.html';
+}
+const sair1=document.getElementById('sair');
+if (sair1) sair1.onclick=sair;
+
+function prova1(){
+    window.location.href='home1.html';
+}
+const prova=document.getElementById('prova');
+if (prova) prova.onclick=prova1;
+
+function github(){
+    window.location.href='https://github.com/RafaelGandolfiG';
+}
+
+const btngit=document.getElementById('github');
+if (btngit) btngit.onclick=github;
+
+function instagram(){
+    window.location.href='https://www.instagram.com/rafael_.gandolfi/';
+}
+
+const btninsta=document.getElementById('instagram');
+if (btninsta) btninsta.onclick=instagram;
+
+function linkedin(){
+    window.location.href='https://www.linkedin.com/in/rafael-gandolfi-146719391/';
+}
+
+const btnlink = document.getElementById('linkedin');
+if (btnlink) btnlink.onclick=linkedin;
+
+function apx(){
+    window.location.href='apx.html';
+}
+
+const btnapx = document.getElementById('apx');
+if (btnapx) btnapx.onclick=apx;
+
+function rbr(){
+    window.location.href='rbr.html';
+}
+
+const btnrbr=document.getElementById('rbr');
+if (btnrbr) btnrbr.onclick=rbr;
+
+function bikcraft(){
+    window.location.href='bikcraft.html';
+}
+
+const btnbike=document.getElementById('bike');
+if (btnbike) btnbike.onclick=bikcraft;
+
+function cor(){
+    window.location.href='corinthians.html';
+}
+
+const btncor=document.getElementById('corinthians')
+if (btncor) btncor.onclick=cor;
+
+function port(){
+    window.location.href='portfolio.html';
+}
+
+const btnport=document.getElementById('port')
+if (btnport) btnport.onclick=port;
