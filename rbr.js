ad9f7884aac00830cf096b3bd51d883c0a8d39fa
Rafael Gandolfi Gonçalves <rafaelgandolfi0@gmail.com>
2026-02-26 17:00:41 -0300

    Add files via upload

---
+++ rbr.js
@@ -0,0 +1,35 @@
+function instagram(){
+    window.location.href='https://www.instagram.com/redbullracing/';
+}
+const btninsta=document.getElementById('instagram');
+if (btninsta) btninsta.onclick=instagram;
+
+function ytb(){
+    window.location.href='https://www.youtube.com/@redbullracing';
+}
+const btnytb=document.getElementById('ytb');
+if (btnytb) btnytb.onclick=ytb;
+
+function twitter(){
+    window.location.href='https://x.com/redbullracing/';
+}
+const btntt=document.getElementById('twitter');
+if (btntt) btntt.onclick=twitter;
+
+function voltar(){
+    window.location.href='home.html';
+}
+const btnvoltar=document.getElementById('voltar');
+if (btnvoltar) btnvoltar.onclick=voltar;
+
+const topo = document.querySelector('.topo');
+
+function ativar(){
+    if (window.scrollY === 0) {
+        topo.classList.remove('ativo');
+    } else {
+        topo.classList.add('ativo');
+    }
+}
+
+window.addEventListener('scroll', ativar)
